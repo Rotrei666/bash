46c17ed6612e1ea7b127e3941ea28a4694172997
rodrimar <ruy_drigo@outlook.com>
2025-07-24 21:08:05 -0600

    Exersice 1

---
+++ exercise_1.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Author: rodrimar
+#Date created: 24 Jul 25
+#Last modified 24 Jul 25
+
+#Description
+#Example how to use comment 
+
+#Usage
+#Practice
+
+tar -cvf ~/Documents/Stuff/Backup_"$(date +%d-%m-%Y_%H-%M-%S)".tar ~/Documents/Stuff 2>/dev/null
--- hello_world.sh
+++ hello_world.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+#Author: rodrimar
+#Date created: 24 Jul 25
+#Last modified 24 Jul 25
+
+#Description
+#Example how to use comment #
+
 echo "Hello World"
+
