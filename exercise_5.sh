b55baa888029ac128272113763ae1f849b63b0f8
rodrimar <ruy_drigo@outlook.com>
2025-08-04 18:25:24 -0600

    a la mitad

--- exercise_5.sh
+++ exercise_5.sh
@@ -5,11 +5,12 @@ read -p "What is your first name?: " name
 read -p "What is your surname/family name?: " last
 
 read -n 4 -s -p "What is your extension number?: " ext
+echo 
 
 read -n 4 -s -p "What access code would you like to use when dialing in?:" pin
+echo
 
-
-echo "$name"
-echo "$last"
-echo "$ext"
-echo "$pin"
+IFS=,
+echo "$name,$last,$ext,$pin" > extensions.csv
+echo
+cat extensions.csv 
